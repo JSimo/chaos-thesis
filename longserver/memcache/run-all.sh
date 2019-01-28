4f6aa4dd3bba849a1d2dde4998b3c50e5762d634
jsimo <jsimo@kth.se>
2019-01-28 15:53:24 +0000

    Fixes argument parsing

--- longserver/memcache/run-all.sh
+++ longserver/memcache/run-all.sh
@@ -1,8 +1,9 @@
 #/usr/bin/env bash
 _MINMIN=""
 _MIN=""
+_PREFIX="\U0001F525"
 
-while getopts ":m" opt; do
+while getopts ":mn" opt; do
   case $opt in
      n)
       _MINMIN="TRUE"
@@ -16,19 +17,22 @@ while getopts ":m" opt; do
   esac
 done
 
+function cprint() {
+    printf "$_PREFIX $1\n\r"
+}
+
 # ------ Cleanup -------
 # Cleanup any old instances of memcached.
 function startMemcache() {
-    echo "Removing old memcaches instances."
+    cprint "Removing old memcaches instances."
     docker container rm --force $(docker container ls --filter name=memcache -q)
 
     # Create memcache containers and run tests
-    echo "Deploying two instances of memcache"
+    cprint "Deploying two instances of memcache"
     docker run -p 11211:11211 -d --name memcached1 memcached:1.5.5
     docker run -p 11212:11211 -d --name memcached2 memcached:1.5.5
 }
 
-echo "Setup"
 _short_date_time="$(date +%Y_%m_%d__%H_%M_%S)"
 _output_dir="results/$_short_date_time"
 mkdir -p "$_output_dir"
@@ -39,19 +43,18 @@ _spym_file="$_output_dir/spym.log"
 touch "$_xmem_file" "$_javam_file" "$_spym_file"
 
 # do stuff
-echo "Starting benchmarks"
+cprint "Starting benchmarks"
 
 function progressbar() {
     bar="##################################################"
     barlength=${#bar}
     n=$(($1*barlength/100))
-    printf "\r[%-${barlength}s (%d%%)] " "${bar:0:n}" "$1"
+    printf "\r$_PREFIX [%-${barlength}s (%d%%)] " "${bar:0:n}" "$1"
 }
 
-
 function runBenchmark() {
-    echo "Running benchmark jar $1 outputting to $2"
-    echo ""
+    cprint "java -jar $1 > $2"
+    #cprint ""
     _jar="$1"
     _output_file="$2"
     DONE_NR_OF_LINES="$3"
@@ -68,19 +71,21 @@ function runBenchmark() {
         _progress=$(($_lines * 100 / $DONE_NR_OF_LINES))
        progressbar "$_progress"
        ps -p $_pid > /dev/null || break #exit loop if process is done.
-       sleep 1
+       sleep 0.2
     done
-    wait $_pid # wait for benchmark process to finish.
+    wait $_pid &> /dev/null # wait for benchmark process to finish.
     progressbar 100 #finished, so for prettyness set progress bar to 100
+    trap - 9 #reset trap handler
+    printf "\n\r"
 }
 
 # jar, output, number of lines in output max.
-if [[ -z "$_MINMIN" ]]; then
+if [[ "$_MINMIN" == "TRUE" ]]; then
     # same a min but with less rounds per type so lesser accuracy.
     runBenchmark xmemcac/xmemcached-minmin.jar "$_xmem_file" 18
     runBenchmark spymemc/spymemcached-minmin.jar "$_spym_file" 18
     runBenchmark javamem/javamemcached-minmin.jar "$_javam_file" 18
-elif  [[ -z "$_MIN" ]]; then
+elif  [[ "$_MIN" == "TRUE" ]]; then
     runBenchmark xmemcac/xmemcached-min.jar "$_xmem_file" 18
     runBenchmark spymemc/spymemcached-min.jar "$_spym_file" 18
     runBenchmark javamem/javamemcached-min.jar "$_javam_file" 18
@@ -91,7 +96,4 @@ else
     runBenchmark javamem/javamemcached.jar "$_javam_file" 20
 fi
 
-echo "Finished benchmarking"
-
-
-
+cprint "Finished benchmarking"
