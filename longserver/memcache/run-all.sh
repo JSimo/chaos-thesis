9517a9602217d7a9a7939897280450f463d6ca88
jsimo <jsimo@kth.se>
2019-01-28 12:39:25 +0000

    update

--- longserver/memcache/run-all.sh
+++ longserver/memcache/run-all.sh
@@ -1,5 +1,17 @@
 #/usr/bin/env bash
-echo "setup"
+# ------ Cleanup -------
+# Cleanup any old instances of memcached.
+function startMemcache() {
+    echo "Removing old memcaches instances."
+    docker container rm --force $(docker container ls --filter name=memcache -q)
+
+    # Create memcache containers and run tests
+    echo "Deploying two instances of memcache"
+    docker run -p 11211:11211 -d --name memcached1 memcached:1.5.5
+    docker run -p 11212:11211 -d --name memcached2 memcached:1.5.5
+}
+
+echo "Setup"
 _short_date_time="$(date +%Y_%m_%d__%H_%M_%S)"
 _output_dir="results/$_short_date_time"
 mkdir -p "$_output_dir"
@@ -12,15 +24,41 @@ touch "$_xmem_file" "$_javam_file" "$_spym_file"
 # do stuff
 echo "Starting benchmarks"
 
-function runBenchmark() {
-    echo "$1 | $2"
-    _jar="$1"
-    _output_file="$2"
-    java -jar "$_jar" > "$_output_file"
+function progressbar() {
+    bar="##################################################"
+    barlength=${#bar}
+    n=$(($1*barlength/100))
+    printf "\r[%-${barlength}s (%d%%)] " "${bar:0:n}" "$1"
 }
 
-runBenchmark xmemcac/xmemcached.jar "$_xmem_file"
-runBenchmark spymemc/spymemcached.jar "$_spym_file"
-runBenchmark javamem/javamemcached.jar "$_javam_file"
+
+function runBenchmark() {
+    echo "Running benchmark jar $1 outputting to $2"
+    echo ""
+    _jar="$1"
+    _output_file="$2"
+    DONE_NR_OF_LINES="$3"
+    startMemcache # start/restart the memcacheinstances
+    sleep 30s # wait for memcache to start
+
+    # start the benchmark jar in the background
+    java -jar "$_jar" &> "$_output_file" &
+    _pid=$!
+    _lines=0
+    while [  $_lines -lt $DONE_NR_OF_LINES ]; do
+        _lines=`cat $_output_file | grep "threads" | wc -l`
+        _progress=$(($_lines * 100 / $DONE_NR_OF_LINES))
+       #echo "progress $_progress lines $_lines done $DONE_NR_OF_LINES"
+       progressbar "$_progress"
+       sleep 1
+    done
+    wait $_pid # wait for benchmark process to finish.
+}
+
+# jar, output, number of lines in output max.
+runBenchmark xmemcac/xmemcached.jar "$_xmem_file" 25
+runBenchmark spymemc/spymemcached.jar "$_spym_file" 25
+runBenchmark javamem/javamemcached.jar "$_javam_file" 20
+
 
 
